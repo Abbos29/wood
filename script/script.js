6d610c02b844a9ea81a75dfedfdfb6576a104ea3
abbos <abbosvaliev2003@gmail.com>
2023-02-09 10:38:01 +0500

    feat: add about, portfolio(swiper) sections

--- script/script.js
+++ script/script.js
@@ -46,3 +46,31 @@ function fixedNav() {
   }
 }
 window.addEventListener('scroll', fixedNav)
+
+
+
+
+// SWIPER
+
+const swiperPortfolio = new Swiper('.portfolio__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // Responsive breakpoints
+    breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+  }
+});
