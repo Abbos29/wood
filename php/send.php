1b90b987c7d8a4cffaf40bd3935027241d571a2d
Farrukh <fzzn777@gmail.com>
2023-03-28 20:33:39 +0500

    rewrite script form

---
+++ php/send.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+$token = "6040481645:AAEhWyfZpa_RtBQ6f16_ysgPAt3fcVJlHb0";
+
+
+$chat_id = "1338691940";
+$chat_id2 = "1300143664";
+
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+);
+
+foreach ($arr as $key => $value) {
+  $txt .= "<b>" . $key . "</b> " . $value . "%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
+$sendToTelegram2 = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
+
+
+if ($sendToTelegram) {
+
+  ob_start();
+  $new_url = 'thanks.html';
+  header('Location: ' . $new_url);
+  ob_end_flush();
+} else {
+  echo "Error";
+}
